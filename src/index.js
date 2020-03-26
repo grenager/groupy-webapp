2b913a528aa2b91e32e819bd1ffe8acc72c02874
radkin <noeldamonmiller@gmail.com>
2020-03-26 12:20:15 -0700

    adding_forms_views partial step
    just a step in the right direction. It is messy, but now we have the
    correct file structure and our gdrawer has been partially implemented

--- src/index.js
+++ src/index.js
@@ -1,46 +1,24 @@
-/*
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { configure } from 'mobx'
+import App from './App';
 
-import GetmeList from './GetmeList'
-import uiState from './uiState'
+// import GetmeList from './GetmeList'
+// import uiState from './uiState'
 import * as serviceWorker from './serviceWorker';
-import Gdrawer from './gdrawer';
 
 configure(true)
 
-// 'response meta tag' for mobile compatibility
-const headElement =
-  <meta
-    content="initial-scale=1, minimum-scale=1, width=device-width"
-    name="viewport"
-  />;
-
 // DOM render tree
-
-// 'responsive meta tag'
-ReactDOM.render(headElement, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // getme apollo client observer
-ReactDOM.render(
-  <GetmeList uiState={uiState} />,
-  document.getElementById('root')
-)
+// ReactDOM.render(
+//   <GetmeList uiState={uiState} />,
+//   document.getElementById('root')
+// )
 
-ReactDOM.render(<Gdrawer />, document.querySelector('#root'));
+// ReactDOM.render(<Gdrawer />, document.querySelector('#root'));
 
 // serviceWorker for offline and load faster
 serviceWorker.unregister();
-
-*/
-
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Main from './Main';
-import './index.css';
-
-ReactDOM.render(
-  <Main/>,
-  document.getElementById('root')
-);
---
+++ src/views/Dashboard/components/index.js
@@ -0,0 +1,3 @@
+export { default as Gdrawer } from './Gdrawer';
+export { default as Home } from './Home';
+export { default as Stuff } from './Stuff';
